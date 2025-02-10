3e74b8dfc532137899a4927c09e38ef1dda3b688
Hafizhan22 <hafizhankhair22@gmail.com>
2025-02-10 21:08:34 +0700

    bikin return

--- api/returnKey.php
+++ api/returnKey.php
@@ -1,48 +1,31 @@
-<?php
+3<?php
 header('Content-Type: application/json');
 include "../application/config/koneksi.php";
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $data = json_decode(file_get_contents("php://input"), true);
     $pic = $data["pic"];
-    $passId = $data["passId"];
-    $purpose = $data["purpose"];
-    $borrowTime = $data["time"];
-    $RFIDlist = $data["keyIdList"];
+    $returnTime = $data["time"];
+    $historyList = $data["historyIdList"];
     $soc = $data["account"];
-
-    // Fungsi untuk mendapatkan ID baru dengan format LOG
-    function getNewId($koneksi) {
-        $sql_getMaxId = mysqli_query($koneksi, "SELECT MAX(id) as max_id FROM history");
-        $row = $sql_getMaxId->fetch_assoc();
-        $maxId = $row['max_id'];
-
-        if ($maxId) {
-            $newIdNum = intval(substr($maxId, 3)) + 1;
-            $newId = 'LOG' . str_pad($newIdNum, 3, '0', STR_PAD_LEFT);
-        } else {
-            $newId = 'LOG001';
-        }
-
-        return $newId;
-    }
-
     $outputSchema = array();
 
-    foreach ($RFIDlist as $RFID) {
-        $sql_selectKey = mysqli_query($koneksi, "SELECT * FROM key_data WHERE RFID = '$RFID'");
+    foreach ($historyList as $idHistory) {
+        $sql_selectHistory = mysqli_query($koneksi, "SELECT * FROM history WHERE ID = '$idHistory'");
+        $row_history = $sql_selectHistory->fetch_assoc();
+        $sql_selectKey = mysqli_query($koneksi, "SELECT * FROM key_data WHERE RFID = '" . $row_history['key_RFID'] . "'");
         if ($sql_selectKey->num_rows > 0) {
             $row_key = $sql_selectKey->fetch_assoc();
-            if ($row_key["status"] == "Available") {
+            if ($row_key["status"] == "Borrowed") {
                 //validasi status kunci
-                $sql_upKey = mysqli_query($koneksi, "UPDATE key_data SET status = 'Borrowed' WHERE RFID = '$RFID'");
+                $sql_upKey = mysqli_query($koneksi, "UPDATE key_data SET status = 'Available' WHERE RFID = '" . $row_history['key_RFID'] . "'");
                 if ($sql_upKey === TRUE) {
-                    $newId = getNewId($koneksi);
-                    $sql_inHistory = mysqli_query($koneksi, "INSERT INTO history VALUES ('$newId', '$passId', '$RFID', '$purpose', '$borrowTime', '$pic', '$soc', '', '', '', 'Active')");
-                    if ($sql_inHistory === TRUE) {
+                    $sql_updateHistory = mysqli_query($koneksi, "UPDATE history SET returnPIC = '$pic', returnTime = '$returnTime', returnSOC = '$soc' WHERE ID = '" . $row_history['ID'] . "'");
+                    $sql_upKey = mysqli_query($koneksi, "UPDATE history SET status = 'Inactive' WHERE ID = '$idHistory'");
+                    if ($sql_updateHistory === TRUE) {
                         $outputSchema[] = array(
-                            "id" => $newId,
-                            "passId" => $passId,
+                            "id" => $row_history["ID"],
+                            "passId" => $row_history["pass_id"],
                             "key" => array(
                                 "rfid" => $row_key["RFID"],
                                 "type" => $row_key["type"],
@@ -50,14 +33,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                 "quantity" => $row_key["quantity"],
                                 "location" => $row_key["location"]
                             ),
-                            "purpose" => $purpose,
-                            "borrowTime" => $borrowTime,
-                            "borrowPic" => $pic,
-                            "borrowSoc" => $soc,
-                            "returnTime" => "",
-                            "returnPic" => "",
-                            "returnSoc" => "",
-                            "status" => "Active"
+                            "purpose" => $row_history["purpose"],
+                            "borrowTime" => $row_history["borrowTime"],
+                            "borrowPic" => $row_history["borrowPIC"],
+                            "borrowSoc" => $row_history["borrowSOC"],
+                            "returnTime" => $row_history["returnTime"],
+                            "returnPic" => $row_history["returnPIC"],
+                            "returnSoc" => $row_history["returnSOC"],
+                            "status" => $row_history["Status"]
                         );
                     } else {
                         echo json_encode(array(
@@ -81,7 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 echo json_encode(array(
                     "errorSchema" => array(
                         "errorCode" => 400,
-                        "errorMessage" => "Key with RFID $RFID is not available."
+                        "errorMessage" => "Key with RFID ". $row_history['key_RFID']. " is not Borrow."
                     )
                 ));
                 exit;
@@ -90,7 +73,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             echo json_encode(array(
                 "errorSchema" => array(
                     "errorCode" => 404,
-                    "errorMessage" => "Key with RFID $RFID not found."
+                    "errorMessage" => "Key with RFID $". $row_history['key_RFID']. " not found."
                 )
             ));
             exit;
